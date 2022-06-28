080ba4bee5a892f7692b5fc39e963441e2e5eb10
H-Huy <honghuy2014vn@gmail.com>
2022-06-28 12:30:29 +0700

    Xong giao dien Tin

--- client/src/screens/DetailPost.js
+++ client/src/screens/DetailPost.js
@@ -6,36 +6,38 @@ import {
   StyleSheet,
   useWindowDimensions,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
 } from "react-native";
 import React, { useState } from "react";
 import GlobalStyles from "../GlobalStyles";
 import Back from "../components/Back";
 import Banner from "../components/Banner";
+import { Avatar } from "react-native-elements";
 import { Ionicons } from "@expo/vector-icons";
 import PublicFolder from "../api/PublicFolder";
 import imageBanner1 from "../../assets/images/banner1.png";
 import LocationScreen from "./LocationScreen";
 import baseURL from "../api/BaseURL";
+import CustomButton from "../components/CustomButton";
+import { stopLocationUpdatesAsync } from "expo-location";
 const DetailPost = ({ route }) => {
   const { width } = useWindowDimensions();
-  const [user , setUser] = useState([]);
-  const getFullnameById = async()=>{
-    try{
-      const response = await fetch (baseURL+`/user/${route.params.userId}`)
-      .then(user=>user.json())
-      .then(userJson=>{
-          setUser(userJson)
-      })
+  const [user, setUser] = useState([]);
+  const getFullnameById = async () => {
+    try {
+      const response = await fetch(baseURL + `/user/${route.params.userId}`)
+        .then((user) => user.json())
+        .then((userJson) => {
+          setUser(userJson);
+        });
+    } catch (error) {
+      console.log(error);
     }
-    catch(error){
-        console.log(error)
-    }
- }
- getFullnameById();
- const handleLocation = () =>{
-    
- }
+  };
+
+  getFullnameById();
+  const handleLocation = () => {};
+  const onPress = () => {};
   return (
     <SafeAreaView style={GlobalStyles.droidSafeArea}>
       <Back textCenter="Chi tiết" />
@@ -60,12 +62,72 @@ const DetailPost = ({ route }) => {
           <Text style={[styles.title, { textTransform: "uppercase" }]}>
             {route.params.title}
           </Text>
-
-          <TouchableOpacity style={{flexDirection : 'row' , alignItems:'center'}} onPress={handleLocation}>
-            <Ionicons name="location-outline" size={30} color={"blue"}></Ionicons>
-            <Text style={[styles.info,{color:"blue"}]}>{route.params.address}</Text>
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              paddingTop: 10,
+            }}
+            onPress={handleLocation}
+          >
+            <Ionicons
+              name="location-outline"
+              size={30}
+              color={"blue"}
+            ></Ionicons>
+            <Text style={[styles.address, { color: "blue" }]}>
+              {route.params.address}
+            </Text>
           </TouchableOpacity>
-          <Text style={styles.info}>{user.fullname}</Text>
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              borderBottomColor: "blue",
+              borderBottomWidth: StyleSheet.hairlineWidth,
+            }}
+            onPress={onPress}
+          >
+            <Avatar
+              size="medium"
+              rounded
+              source={{
+                uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
+              }}
+            ></Avatar>
+            <Text style={styles.info}>{user.fullname}</Text>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.text}>Xem trang</Text>
+            </TouchableOpacity>
+          </TouchableOpacity>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.detail}>{route.params.desc}</Text>
+          </View>
+          <View style={[styles.detail, { flexDirection: "row" }]}>
+            <Ionicons
+              name="navigate-outline"
+              size={20}
+              color={"brown"}
+            ></Ionicons>
+            <Text style={styles.textdetail}>{route.params.address}</Text>
+          </View>
+          <View style={[styles.detail, { flexDirection: "row" }]}>
+            <Ionicons name="grid" size={20} color={"brown"}></Ionicons>
+            <Text style={styles.textdetail}>{route.params.category}</Text>
+          </View>
+          <View style={[styles.detail, { flexDirection: "row" }]}>
+            <Ionicons name="mail" size={20} color={"brown"}></Ionicons>
+            <Text style={styles.textdetail}>
+              {route.params.emailOrgazization}
+            </Text>
+          </View>
+          <View style={[styles.detail, { flexDirection: "row" }]}>
+            <Ionicons name="call" size={20} color={"brown"}></Ionicons>
+            <Text style={styles.textdetail}>{route.params.phonenumber}</Text>
+          </View>
+          <View style={[styles.detail, { flexDirection: "row" }]}>
+            <Ionicons name="globe" size={20} color={"brown"}></Ionicons>
+            <Text style={styles.textdetail}>{route.params.website}</Text>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -92,15 +154,49 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingLeft: 5,
     fontSize: 26,
+    borderBottomColor: "blue",
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
-  info: {
+  address: {
     paddingTop: 10,
     paddingLeft: 5,
+    fontSize: 20,
+  },
+  info: {
+    paddingTop: 20,
+    paddingLeft: 50,
     fontSize: 20,
   },
   img: {
     width: "100%",
     height: "100%",
+  },
+  button: {
+    paddingTop: 20,
+    position: "relative",
+    left: 160,
+    bottom: 5,
+  },
+  text: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "green",
+    color: "white",
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+  },
+  detail: {
+    fontSize: 14,
+    paddingLeft: 20,
+    paddingTop: 10,
+  },
+  textdetail: {
+    paddingLeft: 5,
   },
 });
 
